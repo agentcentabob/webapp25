738477a3a72f74d2462b53827cdac3a3a852b875
agentcentabob <sub2acb@outlook.com>
2025-10-04 19:01:01 +1000

    Started developing service worker

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,54 +1,39 @@
-const assets = [
-  "/",
-  "static/css/style.css",
-  "static/js/app.js",
-  "static/images/logos/favicon_1_4.png",
-  "static/images/logos/full_1.png",
-  "static/images/logos/main_1_3.png",
-  "static/images/westmead_lr.png",
+const CACHE_NAME = "urbannotes-cache1";
+const urlsToCache = [
+  '/',
+  '/static/css/style.css',
+  '/static/js/app.js',
+  '/static/manifest.json'
 ];
 
-const CATALOGUE_ASSETS = "catalogue-assets";
-
-self.addEventListener("install", (installEvt) => {
-  installEvt.waitUntil(
-    caches
-      .open(CATALOGUE_ASSETS)
-      .then((cache) => {
-        console.log(cache);
-        cache.addAll(assets);
-      })
-      .then(self.skipWaiting())
-      .catch((e) => {
-        console.log(e);
-      })
+// install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())  // activates immediately
   );
 });
 
-self.addEventListener("activate", function (evt) {
-  evt.waitUntil(
-    caches
-      .keys()
-      .then((keyList) => {
-        return Promise.all(
-          keyList.map((key) => {
-            if (key === CATALOGUE_ASSETS) {
-              console.log("Removed old cache from", key);
-              return caches.delete(key);
-            }
-          })
-        );
-      })
-      .then(() => self.clients.claim())
+// fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
 
-self.addEventListener("fetch", function (evt) {
-  evt.respondWith(
-    fetch(evt.request).catch(() => {
-      return caches.open(CATALOGUE_ASSETS).then((cache) => {
-        return cache.match(evt.request);
-      });
-    })
+// activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())  // take control immediately
   );
 });
