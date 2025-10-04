a5db74a1abf737e253f705a42ade07e015e908e1
agentcentabob <sub2acb@outlook.com>
2025-10-04 19:27:37 +1000

    Implemented service worker

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,9 @@
-if ("serviceworker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceworker
-      .register("js/serviceworker.js")
-      .then((res) => console.log("service worker registered"))
-      .catch((err) => console.log("service worker not registered", err));
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/static/js/serviceworker.js") 
+      .then(res => console.log("Service Worker registered!", res))
+      .catch(err => console.log("Service Worker registration failed:", err));
   });
 }
 
